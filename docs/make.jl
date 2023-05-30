0d78565fc2df50ad9b854c7c4dc7f1c903c110a6
aconsidera <andre.luis@impa.br>
2023-05-30 04:49:50 +0200

    actions

--- docs/make.jl
+++ docs/make.jl
@@ -6,20 +6,15 @@ DocMeta.setdocmeta!(MultiFractalFields, :DocTestSetup, :(using MultiFractalField
 
 makedocs(
     modules=[MultiFractalFields],
-    authors="Andre Considera",
-    repo="https://github.com/AndreConsidera/MultiFractalFields.jl/blob/{commit}{path}#{line}",
-    sitename="MultiFractalFields.jl",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical="https://AndreConsidera.github.io/MultiFractalFields.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages = [
-        "Home" => "index.md",
-        "API" => "API.md"
-    ],
-    checkdocs=:exports
+    #authors="Andre Considera",
+    #repo="https://github.com/AndreConsidera/MultiFractalFields.jl/blob/{commit}{path}#{line}",
+    sitename="MultiFractalFields",
+    format = Documenter.HTML(),
+    #pages = [
+    #    "Home" => "index.md",
+    #    "API" => "API.md"
+    #],
+    #checkdocs=:exports
 )
 
 deploydocs(
